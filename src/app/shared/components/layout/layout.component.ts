87021bbf75a94ce03a50af895bb6a5701b01c0fc
Ever2A <ever.arones.ayala@gmail.com>
2024-09-16 21:39:55 -0500

    dashboard terminado

--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { FooterComponent } from '../footer/footer.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    RouterOutlet
+],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
-export class LayoutComponent {
+
+export default class LayoutComponent {
 
 }
